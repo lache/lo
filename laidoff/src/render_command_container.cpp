5e4f0f679b3f4b521f76edaf2d761a7fede4bd99
Geoyeob Kim <gasbank@msn.com>
2018-07-02 15:45:16 +0900

    preparing code to support htmlui render queue

--- laidoff/src/render_command_container.cpp
+++ laidoff/src/render_command_container.cpp
@@ -1,4 +1,4 @@
-#include "text_container.h"
+#include "render_command_container.h"
 #include <stdio.h>
 #include <locale>
 #include "lwgl.h"
@@ -16,35 +16,27 @@
 #include "remtex.h"
 #include "logic.h"
 
-litehtml::text_container::text_container(LWCONTEXT* pLwc, int w, int h)
+litehtml::render_command_container::render_command_container(LWCONTEXT* pLwc, int w, int h)
     : pLwc(pLwc) {
     set_client_size(w, h);
 }
 
-litehtml::text_container::~text_container() {
+litehtml::render_command_container::~render_command_container() {
 }
 
-litehtml::uint_ptr litehtml::text_container::create_font(const litehtml::tchar_t * faceName, int size, int weight, litehtml::font_style italic, unsigned int decoration, litehtml::font_metrics * fm) {
+litehtml::uint_ptr litehtml::render_command_container::create_font(const litehtml::tchar_t * faceName, int size, int weight, litehtml::font_style italic, unsigned int decoration, litehtml::font_metrics * fm) {
     LOGIx("create_font: faceName=%s, size=%d, weight=%d\n", faceName, size, weight);
     size_t font_idx = font_sizes.size();
     font_sizes.push_back(size);
-    if (client_aspect_ratio > 1) {
-        //fm->height = static_cast<int>(roundf(size * 0.8f * client_height / 720.0f));
-        //fm->descent = static_cast<int>(roundf(size * 0.1f * client_height / 720.0f));
-    } else {
-        //fm->height = static_cast<int>(roundf(size * 0.8f * client_width / 1280.0f));
-        //fm->descent = static_cast<int>(roundf(size * 0.1f * client_width / 1280.0f));
-    }
-    fm->height = static_cast<int>(size / 5.5f);// static_cast<int>(roundf(size * 0.8f * client_height / 720.0f));
+    fm->height = static_cast<int>(size / 5.5f);
     fm->descent = static_cast<int>(fm->height / 4.5f);
-    //fm->ascent = fm->height / 4;
-    return litehtml::uint_ptr(font_idx);// litehtml::uint_ptr();
+    return litehtml::uint_ptr(font_idx);
 }
 
-void litehtml::text_container::delete_font(litehtml::uint_ptr hFont) {
+void litehtml::render_command_container::delete_font(litehtml::uint_ptr hFont) {
 }
 
-void litehtml::text_container::fill_text_block(LWTEXTBLOCK* text_block, int x, int y, const char* text, int size, const litehtml::web_color& color) {
+void litehtml::render_command_container::fill_text_block(LWTEXTBLOCK* text_block, int x, int y, const char* text, int size, const litehtml::web_color& color) {
     text_block->text_block_width = 999.0f;
     LOGIx("font size: %d", size);
     text_block->text_block_line_height = static_cast<float>(size);
@@ -63,40 +55,36 @@ void litehtml::text_container::fill_text_block(LWTEXTBLOCK* text_block, int x, i
     text_block->align = LTBA_LEFT_BOTTOM;
 }
 
-int litehtml::text_container::text_width(const litehtml::tchar_t * text, litehtml::uint_ptr hFont) {
+int litehtml::render_command_container::text_width(const litehtml::tchar_t * text, litehtml::uint_ptr hFont) {
     LWTEXTBLOCK text_block;
     LWTEXTBLOCKQUERYRESULT query_result;
     int size = font_sizes[(int)(size_t)hFont];
     litehtml::web_color c;
     fill_text_block(&text_block, 0, 0, text, size, c);
     render_query_only_text_block(pLwc, &text_block, &query_result);
-    //return static_cast<int>(query_result.total_glyph_width / (2 * client_rt_x) * client_width);
     return static_cast<int>(query_result.total_glyph_width);
 }
 
-void litehtml::text_container::draw_text(litehtml::uint_ptr hdc, const litehtml::tchar_t * text, litehtml::uint_ptr hFont, litehtml::web_color color, const litehtml::position & pos) {
-    //wprintf(_t("draw_text: '%s' (x=%d,y=%d,color=0x%02X%02X%02X|%02X)\n"), text, pos.x, pos.y, color.red, color.green, color.blue, color.alpha);
+void litehtml::render_command_container::draw_text(litehtml::uint_ptr hdc, const litehtml::tchar_t * text, litehtml::uint_ptr hFont, litehtml::web_color color, const litehtml::position & pos) {
     int size = font_sizes[(int)(size_t)hFont];
     LWTEXTBLOCK text_block;
     fill_text_block(&text_block, pos.x, pos.y, text, size, color);
-    //render_text_block(pLwc, &text_block);
     render_text_block_two_pass_color(pLwc, &text_block);
 }
 
-int litehtml::text_container::pt_to_px(int pt) {
-    //return static_cast<int>(roundf(pt * 3.6f * client_width / 640.0f));
+int litehtml::render_command_container::pt_to_px(int pt) {
     return pt * 3;
 }
 
-int litehtml::text_container::get_default_font_size() const {
+int litehtml::render_command_container::get_default_font_size() const {
     return default_font_size;
 }
 
-const litehtml::tchar_t * litehtml::text_container::get_default_font_name() const {
+const litehtml::tchar_t * litehtml::render_command_container::get_default_font_name() const {
     return _t("Arial");
 }
 
-void litehtml::text_container::draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_marker & marker) {
+void litehtml::render_command_container::draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_marker & marker) {
     LOGIx("draw_list_marker");
     int size = font_sizes[0];
     LWTEXTBLOCK text_block;
@@ -104,8 +92,7 @@ void litehtml::text_container::draw_list_marker(litehtml::uint_ptr hdc, const li
     render_text_block(pLwc, &text_block);
 }
 
-void litehtml::text_container::load_image(const litehtml::tchar_t * src, const litehtml::tchar_t * baseurl, bool redraw_on_ready) {
-    //wprintf(_t("load_image: src=%s,baseurl=%s,redraw_on_ready=%d\n"), src, baseurl, redraw_on_ready);
+void litehtml::render_command_container::load_image(const litehtml::tchar_t * src, const litehtml::tchar_t * baseurl, bool redraw_on_ready) {
 }
 
 static LWATLASSPRITEPTR atlas_sprite_ptr_from_url(const LWCONTEXT* pLwc, const std::string& src) {
@@ -143,7 +130,7 @@ static void separate_alpha_texture(const char* src, char* alpha_src, size_t max_
     }
 }
 
-void litehtml::text_container::get_image_size(const litehtml::tchar_t * src, const litehtml::tchar_t * baseurl, litehtml::size & sz) {
+void litehtml::render_command_container::get_image_size(const litehtml::tchar_t * src, const litehtml::tchar_t * baseurl, litehtml::size & sz) {
     LOGIx("get_image_size: src=%s,baseurl=%s", src, baseurl);
     // [1] check for 'atlas type' image
     LWATLASSPRITEPTR atlas_sprite_ptr = atlas_sprite_ptr_from_url(pLwc, src);
@@ -191,7 +178,7 @@ void litehtml::text_container::get_image_size(const litehtml::tchar_t * src, con
     }
 }
 
-void litehtml::text_container::draw_background(litehtml::uint_ptr hdc, const litehtml::background_paint & bg) {
+void litehtml::render_command_container::draw_background(litehtml::uint_ptr hdc, const litehtml::background_paint & bg) {
     // ignore body background-color (only applied on browser-testing phase)
     if (bg.is_root) {
         return;
@@ -205,6 +192,10 @@ void litehtml::text_container::draw_background(litehtml::uint_ptr hdc, const lit
     int lae_alpha = LAE_TTL_TITLE_ALPHA;
 
     int show_test_image = 0;
+    // 1: test image
+    // 2: slice-test.png
+    // 3: atlas
+    // 4: remtex
 
     if (bg.image.length()) {
         LOGIx("draw_background [IMAGE]: x=%d,y=%d,w=%d,h=%d,clipbox=%d/%d/%d/%d,position_xy=%d/%d,image_size=%d/%d,color=0x%02X%02X%02X|%02X,image=%s,baseurl=%s",
@@ -371,7 +362,7 @@ void litehtml::text_container::draw_background(litehtml::uint_ptr hdc, const lit
     }
 }
 
-void litehtml::text_container::draw_border_rect(const litehtml::border& border, int x, int y, int w, int h, LW_VBO_TYPE lvt, const litehtml::web_color& color) const {
+void litehtml::render_command_container::draw_border_rect(const litehtml::border& border, int x, int y, int w, int h, LW_VBO_TYPE lvt, const litehtml::web_color& color) const {
     if (w <= 0 || h <= 0) {
         return;
     }
@@ -396,7 +387,7 @@ void litehtml::text_container::draw_border_rect(const litehtml::border& border, 
     );
 }
 
-void litehtml::text_container::draw_borders(litehtml::uint_ptr hdc, const litehtml::borders & borders, const litehtml::position & draw_pos, bool root) {
+void litehtml::render_command_container::draw_borders(litehtml::uint_ptr hdc, const litehtml::borders & borders, const litehtml::position & draw_pos, bool root) {
     LOGIx("draw_borders: x=%d y=%d w=%d h=%d Lw=%d Rw=%d Tw=%d Bw=%d root=%d",
           draw_pos.x,
           draw_pos.y,
@@ -417,23 +408,23 @@ void litehtml::text_container::draw_borders(litehtml::uint_ptr hdc, const liteht
     draw_border_rect(borders.bottom, draw_pos.x, draw_pos.y + draw_pos.height, draw_pos.width, borders.bottom.width, LVT_LEFT_BOTTOM_ANCHORED_SQUARE, borders.bottom.color);
 }
 
-void litehtml::text_container::set_caption(const litehtml::tchar_t * caption) {
+void litehtml::render_command_container::set_caption(const litehtml::tchar_t * caption) {
     //wprintf(_t("set_caption: %s\n"), caption);
 }
 
-void litehtml::text_container::set_base_url(const litehtml::tchar_t * base_url) {
+void litehtml::render_command_container::set_base_url(const litehtml::tchar_t * base_url) {
     //wprintf(_t("set_base_url: %s\n"), base_url);
 }
 
-void litehtml::text_container::link(const std::shared_ptr<litehtml::document>& doc, const litehtml::element::ptr & el) {
+void litehtml::render_command_container::link(const std::shared_ptr<litehtml::document>& doc, const litehtml::element::ptr & el) {
     //printf("link\n");
 }
 
-void litehtml::text_container::on_anchor_click(const litehtml::tchar_t * url, const litehtml::element::ptr & el) {
+void litehtml::render_command_container::on_anchor_click(const litehtml::tchar_t * url, const litehtml::element::ptr & el) {
     on_anchor_click_ex(url, el, true);
 }
 
-void litehtml::text_container::on_anchor_click_ex(const litehtml::tchar_t * url, const litehtml::element::ptr & el, bool add_touch_rect) {
+void litehtml::render_command_container::on_anchor_click_ex(const litehtml::tchar_t * url, const litehtml::element::ptr & el, bool add_touch_rect) {
     LOGIx("on_anchor_click_ex: %s", url);
     auto global_position = el->get_position();
     litehtml::element::ptr el_recursive = el;
@@ -503,27 +494,27 @@ void litehtml::text_container::on_anchor_click_ex(const litehtml::tchar_t * url,
     }
 }
 
-void litehtml::text_container::set_cursor(const litehtml::tchar_t * cursor) {
+void litehtml::render_command_container::set_cursor(const litehtml::tchar_t * cursor) {
     //printf("set_cursor\n");
 }
 
-void litehtml::text_container::transform_text(litehtml::tstring & text, litehtml::text_transform tt) {
+void litehtml::render_command_container::transform_text(litehtml::tstring & text, litehtml::text_transform tt) {
     //printf("transform_text\n");
 }
 
-void litehtml::text_container::import_css(litehtml::tstring & text, const litehtml::tstring & url, litehtml::tstring & baseurl) {
+void litehtml::render_command_container::import_css(litehtml::tstring & text, const litehtml::tstring & url, litehtml::tstring & baseurl) {
     //printf("import_css\n");
 }
 
-void litehtml::text_container::set_clip(const litehtml::position & pos, const litehtml::border_radiuses & bdr_radius, bool valid_x, bool valid_y) {
+void litehtml::render_command_container::set_clip(const litehtml::position & pos, const litehtml::border_radiuses & bdr_radius, bool valid_x, bool valid_y) {
     //printf("set_clip\n");
 }
 
-void litehtml::text_container::del_clip() {
+void litehtml::render_command_container::del_clip() {
     //printf("del_clip\n");
 }
 
-void litehtml::text_container::get_client_rect(litehtml::position & client) const {
+void litehtml::render_command_container::get_client_rect(litehtml::position & client) const {
     client.x = 0;
     client.y = 0;
     client.width = client_width;
@@ -531,7 +522,7 @@ void litehtml::text_container::get_client_rect(litehtml::position & client) cons
     //printf("get_client_rect\n");
 }
 
-std::shared_ptr<litehtml::element> litehtml::text_container::create_element(const litehtml::tchar_t * tag_name, const litehtml::string_map & attributes, const std::shared_ptr<litehtml::document>& doc) {
+std::shared_ptr<litehtml::element> litehtml::render_command_container::create_element(const litehtml::tchar_t * tag_name, const litehtml::string_map & attributes, const std::shared_ptr<litehtml::document>& doc) {
     if (strcmp(tag_name, "script") == 0) {
         auto ait = attributes.find("type");
         if (ait != attributes.cend() && ait->second == "text/x-lua") {
@@ -541,13 +532,13 @@ std::shared_ptr<litehtml::element> litehtml::text_container::create_element(cons
     return std::shared_ptr<litehtml::element>();
 }
 
-void litehtml::text_container::get_media_features(litehtml::media_features & media) const {
+void litehtml::render_command_container::get_media_features(litehtml::media_features & media) const {
 }
 
-void litehtml::text_container::get_language(litehtml::tstring & language, litehtml::tstring & culture) const {
+void litehtml::render_command_container::get_language(litehtml::tstring & language, litehtml::tstring & culture) const {
 }
 
-void litehtml::text_container::set_client_size(int client_width, int client_height) {
+void litehtml::render_command_container::set_client_size(int client_width, int client_height) {
     this->client_width = client_width;
     this->client_height = client_height;
     this->client_aspect_ratio = (float)client_width / client_height;
---
+++ laidoff/src/render_command_container.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "litehtml.h"
+#include <vector>
+#include <set>
+#include "lwvbotype.h"
+
+typedef struct _LWCONTEXT LWCONTEXT;
+typedef struct _LWTEXTBLOCK LWTEXTBLOCK;
+namespace litehtml {
+	class render_command_container : public document_container {
+	public:
+		typedef std::map<std::wstring, litehtml::uint_ptr>	images_map;
+
+	protected:
+
+		images_map					m_images;
+		litehtml::position::vector	m_clips;
+		std::vector<int> font_sizes;
+
+	public:
+		render_command_container(LWCONTEXT* pLwc, int w, int h);
+		virtual ~render_command_container();
+
+		virtual litehtml::uint_ptr	create_font(const litehtml::tchar_t* faceName, int size, int weight, litehtml::font_style italic, unsigned int decoration, litehtml::font_metrics* fm);
+		virtual void				delete_font(litehtml::uint_ptr hFont);
+		virtual int					text_width(const litehtml::tchar_t* text, litehtml::uint_ptr hFont);
+		virtual void				draw_text(litehtml::uint_ptr hdc, const litehtml::tchar_t* text, litehtml::uint_ptr hFont, litehtml::web_color color, const litehtml::position& pos);
+		virtual int					pt_to_px(int pt);
+		virtual int					get_default_font_size() const;
+		virtual const litehtml::tchar_t*	get_default_font_name() const;
+		virtual void				draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_marker& marker);
+		virtual void				load_image(const litehtml::tchar_t* src, const litehtml::tchar_t* baseurl, bool redraw_on_ready);
+		virtual void				get_image_size(const litehtml::tchar_t* src, const litehtml::tchar_t* baseurl, litehtml::size& sz);
+		virtual void				draw_background(litehtml::uint_ptr hdc, const litehtml::background_paint& bg);
+		virtual void				draw_borders(litehtml::uint_ptr hdc, const litehtml::borders& borders, const litehtml::position& draw_pos, bool root);
+
+		virtual	void				set_caption(const litehtml::tchar_t* caption);
+		virtual	void				set_base_url(const litehtml::tchar_t* base_url);
+		virtual void				link(const std::shared_ptr<litehtml::document>& doc, const litehtml::element::ptr& el);
+		virtual void				on_anchor_click(const litehtml::tchar_t* url, const litehtml::element::ptr& el);
+		virtual	void				set_cursor(const litehtml::tchar_t* cursor);
+		virtual	void				transform_text(litehtml::tstring& text, litehtml::text_transform tt);
+		virtual void				import_css(litehtml::tstring& text, const litehtml::tstring& url, litehtml::tstring& baseurl);
+		virtual void				set_clip(const litehtml::position& pos, const litehtml::border_radiuses& bdr_radius, bool valid_x, bool valid_y);
+		virtual void				del_clip();
+		virtual void				get_client_rect(litehtml::position& client) const;
+		virtual std::shared_ptr<litehtml::element>	create_element(const litehtml::tchar_t *tag_name,
+			const litehtml::string_map &attributes,
+			const std::shared_ptr<litehtml::document> &doc);
+
+		virtual void				get_media_features(litehtml::media_features& media) const;
+		virtual void				get_language(litehtml::tstring& language, litehtml::tstring & culture) const;
+
+        void set_online(bool b) { online = b; }
+        void clear_remtex_name_hash_set() { remtex_name_hash_set.clear(); }
+        bool need_update_on_remtex_change(unsigned long name_hash) const {
+            return remtex_name_hash_set.find(name_hash) != remtex_name_hash_set
+                .end();
+        }
+        void set_client_size(int client_width, int client_height);
+        void on_anchor_click_ex(const litehtml::tchar_t* url, const litehtml::element::ptr& el, bool add_touch_rect);
+	private:
+        void draw_border_rect(const litehtml::border& border, int x, int y, int w, int h, LW_VBO_TYPE lvt, const litehtml::web_color& color) const;
+        float conv_size_x(int x) const { return static_cast<float>(x); }// return 2 * ((float)x / client_width * client_rt_x); }
+        float conv_size_y(int y) const { return static_cast<float>(y); }// return 2 * ((float)y / client_height * client_rt_y); }
+        float conv_coord_x(int x) const { return static_cast<float>(x); }// return -client_rt_x + conv_size_x(x); }
+        float conv_coord_y(int y) const { return client_height - static_cast<float>(y); }// return client_rt_y - conv_size_y(y); }
+        void fill_text_block(LWTEXTBLOCK* text_block, int x, int y, const char* text, int size, const litehtml::web_color& color);
+        LWCONTEXT * pLwc;
+		int client_width;
+		int client_height;
+        float client_aspect_ratio;
+        float client_rt_x;
+        float client_rt_y;
+		int default_font_size;
+        bool online;
+        std::set<unsigned long> remtex_name_hash_set;
+	};
+}
