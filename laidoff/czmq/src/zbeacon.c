5424132f9e2c2bbb18cf29b173385ef87a677c65
Geoyeob Kim <gasbank@gmail.com>
2018-08-06 22:47:54 +0900

    windows build fix

--- laidoff/czmq/src/zbeacon.c
+++ laidoff/czmq/src/zbeacon.c
@@ -29,11 +29,30 @@
 
 //#include "platform.hpp"
 //
-//#ifdef IOS
-//#include <net/if.h>
-//#endif
-unsigned int if_nametoindex (const char *ifname);
-char *if_indextoname (unsigned int ifindex, char *ifname);
+#ifdef WIN32
+#    define LW_PLATFORM_WIN32 1
+#elif __APPLE__
+#    include <TargetConditionals.h>
+#    if TARGET_OS_IPHONE && TARGET_IPHONE_SIMULATOR
+#        define LW_PLATFORM_IOS 1
+#        define LW_PLATFORM_IOS_SIMULATOR 1
+#    elif TARGET_OS_IPHONE
+#        define LW_PLATFORM_IOS 1
+#    else
+#        define LW_PLATFORM_OSX 1
+#    endif
+#elif RPI
+#    define LW_PLATFORM_RPI 1
+#elif __ANDROID__
+#    define LW_PLATFORM_ANDROID 1
+#elif __linux__
+#    define LW_PLATFORM_LINUX 1
+#else
+#    define LW_PLATFORM_ANDROID 1
+#endif
+#ifdef LW_PLATFORM_IOS
+#include <net/if.h>
+#endif
 
 //  Constants
 #define INTERVAL_DFLT  1000         //  Default interval = 1 second
