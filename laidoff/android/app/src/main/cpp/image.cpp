4a3de2493fee34a119d41920e12fd5c8fbd385de
Geoyeob Kim <gasbank@gmail.com>
2020-03-04 19:34:15 +0900

    천만년만의 커밋. 안드로이드 빌드 ㄱㄱ

--- laidoff/android/app/src/main/cpp/image.cpp
+++ laidoff/android/app/src/main/cpp/image.cpp
@@ -6,6 +6,7 @@
 // TODO
 #else
 #include <android/log.h>
+#include <string.h>
 #define LOGV(...) ((void)__android_log_print(ANDROID_LOG_VERBOSE, "native-activity", __VA_ARGS__))
 #define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "native-activity", __VA_ARGS__))
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
